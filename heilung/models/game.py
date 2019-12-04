e2a2a1a0ab53ed0a23b0834ba2674c0e018daedf
Lennart Purucker <lennart.purucker@t-online.de>
2019-12-04 15:41:19 +0100

    add property to game object which lists all encountered pathogens

--- heilung/models/game.py
+++ heilung/models/game.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 import networkx as nx
 
 from .city import City
+from .events.sub_event.pathogen import Pathogen
 
 
 class Game:
@@ -27,7 +28,7 @@ class Game:
                  for to_city in city.connections]
         self.connections.add_edges_from(edges)
 
-    def state_recap(self):
+    def state_recap(self, short=False):
         """
         Help function to get a easily human readable output of the current state (filtered for only infected cities)
         :return: String of the state recap
@@ -35,16 +36,18 @@ class Game:
         overview = "Outcome: %s | Round: %s | Points: %s" % (self.outcome, self.round, self.points)
         error = "Error MSG: %s" % self.error
         game_events = "Game events: %s" % str(self.events)
-        infected_cities = "-Infected Cities- \n"
-        for city, city.outbreak in self.outbreaks:
-            # Build overview of infected city
-            outbreak = city.outbreak
-            pathogen = city.outbreak.pathogen
-            tmp_string = "City Name: %s \n \t SinceRound: %s, Prevalence: %s \n \t Pathogen Name: %s \n \t " \
-                         "Infectivity: %s, Mobility: %s, Duration: %s, Lethality: %s \n" % \
-                         (city.name, outbreak.sinceRound, outbreak.prevalence, pathogen.name, pathogen.infectivity,
-                          pathogen.mobility, pathogen.duration, pathogen.lethality)
-            infected_cities = infected_cities + tmp_string
+        infected_cities = ""
+        if not short:
+            infected_cities = "-Infected Cities- \n"
+            for city, city.outbreak in self.outbreaks:
+                # Build overview of infected city
+                outbreak = city.outbreak
+                pathogen = city.outbreak.pathogen
+                tmp_string = "City Name: %s \n \t SinceRound: %s, Prevalence: %s \n \t Pathogen Name: %s \n \t " \
+                             "Infectivity: %s, Mobility: %s, Duration: %s, Lethality: %s \n" % \
+                             (city.name, outbreak.sinceRound, outbreak.prevalence, pathogen.name, pathogen.infectivity,
+                              pathogen.mobility, pathogen.duration, pathogen.lethality)
+                infected_cities = infected_cities + tmp_string
 
         # TODO add city events, city stats, city flightpahts
 
@@ -65,8 +68,17 @@ class Game:
         return tmp_list
 
     @property
+    def encountered_pathogens(self) -> List[Pathogen]:
+        """
+        Builds a list of already encountered pathogens
+        :return: List[Pathogen]
+        """
+        return [Pathogen(event['pathogen']['name'], event['pathogen']['infectivity'], event['pathogen']['mobility'],
+                         event['pathogen']['duration'], event['pathogen']['lethality']) for event in self.events if
+                event['type'] == 'pathogenEncountered']
+
+    @property
     def city_events(self) -> List[Tuple[City, dict]]:
         return [(city, city.events) for city in self.cities.values() if len(city.events) > 0]
-
 
 # TODO get list of infected cities
