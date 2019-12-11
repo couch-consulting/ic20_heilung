8fd15fcb492876e0307149cb31bed237e48cb86d
Lennart Purucker <lennart.purucker@t-online.de>
2019-12-11 23:18:12 +0100

    fixed docu typo

--- heilung/models/events/quarantine.py
+++ heilung/models/events/quarantine.py
@@ -2,14 +2,13 @@ from heilung.models.event import Event
 
 
 class Quarantine(Event):
-    """Model of an connection closed event
+    """Model of a quarantined event
     """
 
     def __init__(self, sinceRound: int, untilRound: int):
         """Creates a new event object
 
         Arguments:
-            city {str} -- Name (!) of the city
             sinceRound {int} -- Start Round
             untilRound {int} -- End Round
         """
