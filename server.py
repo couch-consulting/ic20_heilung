ef97007e8c70a1a204ddf07b23dea24b69f6e3ae
Lennart Purucker <lennart.purucker@t-online.de>
2019-12-18 00:30:28 +0100

    cleaned server.py

--- server.py
+++ server.py
@@ -28,12 +28,12 @@ def index():
     action_builder = ActionBuilder(game)
     action_list = action_builder.get_actions()
 
-    # Heuristic response
-    human = Human(game)
-    response = human.get_decision()
+    # # Heuristic response
+    # human = Human(game)
+    # response = human.get_decision()
 
     # Example for random iterations
-    # response = action_builder.random_action(action_list)
+    response = action_builder.random_action(action_list)
 
     return response.build_action()
 
